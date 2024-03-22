1be58c9d6d690c58e981e89c13179ff2ea262b7d
Nilabhra <nilabhraroy@gmail.com>
2024-03-22 15:09:52 +0400

    feat: options for stylua

--- nvim/lua/plugins/none-ls.lua
+++ nvim/lua/plugins/none-ls.lua
@@ -6,15 +6,24 @@ return {
 		local null_ls = require("null-ls")
 		null_ls.setup({
 			sources = {
-				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.stylua.with({
+					extra_args = {
+						"--indent_type",
+						"Spaces",
+						"--indent_width",
+						"2",
+					},
+				}),
 				null_ls.builtins.formatting.black,
 				null_ls.builtins.formatting.isort.with({
-					args = {"--profile",
-					"black",
-					"--stdout",
-					"--filename",
-					"$FILENAME",
-					"-",}
+					args = {
+						"--profile",
+						"black",
+						"--stdout",
+						"--filename",
+						"$FILENAME",
+						"-",
+					},
 				}),
 				-- null_ls.builtins.formatting.ruff,
 			},
